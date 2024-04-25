92e852722368d73e347dc3e22cf6d94981375712
ddbecerragarcia <danielabecerra12345@gmail.com>
2024-04-25 10:18:04 -0700

    intital

---
+++ sketch.js
@@ -0,0 +1,42 @@
+let font;
+let string = `
+I’m the wolf.
+Alexander T. Wolf.
+You can call me Al.`;
+let img;
+var index = 0;
+var last = 0;
+
+function preload() {
+  img = loadImage('wolf1.png');
+}
+
+function setup() {
+  createCanvas(600, 600);
+  textSize(40);
+  textAlign(CENTER, CENTER);
+  textFont('Barriecito');
+  
+}
+
+function draw() {
+  typeWriter();
+}
+
+function typeWriter() {
+  background('#C6E1AC');
+  text(string.substring(0, index), 0, 0, width, height);
+	
+	
+	if (millis() > last + 200) {
+		index = index + 1;
+		//ONE WORD AT A TIME
+		// while(message.charAt(index) != ' ' &&
+		// 		 index < message.length){
+		// 	index = index + 1;
+		// }
+		last = millis();
+	}
+  image(img, 150, 85);
+
+}
