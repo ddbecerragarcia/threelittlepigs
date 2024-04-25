568afc68580103ebb97339bb0fcfc462cf4908e4
ddbecerragarcia <danielabecerra12345@gmail.com>
2024-04-25 10:52:09 -0700

    push

---
+++ sketch2.js
@@ -0,0 +1,37 @@
+let value = 0;
+let img;
+
+function preload() {
+  img = loadImage('wolf1.png');
+}
+
+function setup() {
+  createCanvas(600, 600);
+  background('#C6E1AC');
+  fill(0);
+  textFont(`Barriecito`);
+  textSize(20);
+  text("I don’t know how this whole Big Bad Wolf thing got started,", 60,     288);
+}
+
+function draw() {
+  image(img, 150, 85);
+  fill(value);
+  textFont(`Barriecito`);
+  textSize(20);
+  // text("I don’t know how this whole Big Bad Wolf thing got started,", 60,     288);
+}
+
+function keyTyped() {
+  if (key === 'w') {
+    value = '#EE7179';
+  } else if (key === 'o') {
+    value = 0;
+  } else if (key === 'l') {
+    value = 250;
+  } else if (key === 'f') {
+    value = 0;
+  }
+  textSize(65);
+  text("but it’s all wrong.", 70, 350);
+}
